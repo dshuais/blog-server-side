dac9dba16992b1c9dbcb1683839ae530fcb40cf0
dushuais <1137896420@qq.com>
2023-05-09 15:50:36 +0800

    fix：修改nodemon

--- routers/hello.router.ts
+++ routers/hello.router.ts
@@ -3,11 +3,12 @@
  * @Author: dushuai
  * @Date: 2023-05-08 16:55:58
  * @LastEditors: dushuai
- * @LastEditTime: 2023-05-08 17:04:30
+ * @LastEditTime: 2023-05-09 15:01:12
  * @description: 心平气和
  */
 import { Context } from "koa"
-const router = require('koa-router')({ prefix: '/hello' })
+const Router = require('koa-router')
+const router = new Router() // ({ prefix: '/hello' })
 
 /** 获取hello数据列表 */
 router.get('/list/foreend', (ctx: Context) => {
