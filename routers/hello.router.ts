96b7119757cba949df7fbbf1589680e07f94974e
dushuais <1137896420@qq.com>
2023-05-09 18:27:44 +0800

    fix

--- routers/hello.router.ts
+++ routers/hello.router.ts
@@ -3,12 +3,11 @@
  * @Author: dushuai
  * @Date: 2023-05-08 16:55:58
  * @LastEditors: dushuai
- * @LastEditTime: 2023-05-09 15:01:12
+ * @LastEditTime: 2023-05-09 18:26:53
  * @description: 心平气和
  */
 import { Context } from "koa"
-const Router = require('koa-router')
-const router = new Router() // ({ prefix: '/hello' })
+const router = require('koa-router')()
 
 /** 获取hello数据列表 */
 router.get('/list/foreend', (ctx: Context) => {
