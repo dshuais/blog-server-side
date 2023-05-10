a094a9c21f706045c954f5da8238b42e8285dc74
dushuais <1137896420@qq.com>
2023-05-10 15:52:01 +0800

    feat:添加db配置等

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-05-10 11:31:53
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-05-10 15:09:35
+ * @description: 数据库配置
+ */
+
+const env: string = process.env.npm_lifecycle_event as string
+
---
+++ models/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-05-10 11:01:15
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-05-10 11:32:30
+ * @description: sequelize配置
+ */
+const { Sequelize } = require('sequelize')
+
+const seq = new Sequelize()
+
+module.exports = seq
